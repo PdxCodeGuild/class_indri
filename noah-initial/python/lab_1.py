14ea03ab59b09271fbc4fad84d00618c1372fafe
IcyStab <87219008+IcyStab@users.noreply.github.com>
2022-10-19 23:35:57 -0500

    unit 1 lab submission

---
+++ noah-initial/python/lab_1.py
@@ -0,0 +1,19 @@
+distances = {
+    "meters":0.3048,
+    "feet":1,
+    "miles": 1609.34,
+    "kilometers":1000,
+    "yards": 0.9144,
+    "inches": 0.0254
+}
+
+
+unit_measured_one = input("What unit would you like to measure?: ")
+unit_measured_two = input(f"What unit would you like to convert {unit_measured_one} to?: ")
+amount_measured = float(input(f"How many {unit_measured_one} would you like to convert to {unit_measured_two}?: "))
+
+unit_measured_one_in_meters = distances[unit_measured_one] * amount_measured
+
+answer = unit_measured_one_in_meters / distances[unit_measured_two]
+
+print(print(f"{amount_measured} {unit_measured_one} converted to {unit_measured_two} is {answer}"))
