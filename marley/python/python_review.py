edb7f5019d0da6d50c24a419bb6d280d95b2a277
MarleyP002 <marley.paul100@gmail.com>
2022-10-19 20:10:01 -0700

    added notes and got rid of gitkeep

--- marley/python/python_review.py
+++ marley/python/python_review.py
@@ -30,9 +30,9 @@ comment
 '''
 
 """
-print('''
-you can print
-a multi line statment
+#print('''
+#you can print
+#a multi line statment
 ''')
 """
 
@@ -101,14 +101,142 @@ else:
     print("this statement was true")
 #elif only the first thing that is true will be executed the rest will be skipped
 """
-
-temp = input("what is the current temperature outside?(using a whole number) ")
-temp = int(temp)
-print(type(temp))
-
-if temp >= 80:
+"""
+temperature = input("what is the current temperature outside?(using a whole number) ")
+temperature = int(temp)
+#print(type(temp))
+#print(temp)
+if temperature >= 80:
     print("its a hot day out today")
-elif temp <= 79 and temp >= 60:
+elif temperature <= 79 and temp >= 60:
     print("its a nice day today")
-elif temp < 60:
+elif temperature < 60:
     print("its cold today")
+"""
+#error handling
+'''
+"""
+temperature = input("what temperature is it outside? ")
+try:
+    temperature = int(temperature)
+except:
+    print("something wrong...")
+    exit()
+if temperature >= 80:
+    print("its a hot day out today")
+elif temperature <= 79 and temp >= 60:
+    print("its a nice day today")
+elif temperature < 60:
+    print("its cold today")
+"""
+#not a good way to handle error just kicks you out if you do something wrong
+'''
+while True:
+    temperature = input("what is the temperature outside? ")
+    try:
+        temperature = int(temperature)
+        break
+    except:
+        print("invalid input enter just the digits of the temp.")
+if temperature >= 80:
+    print("its a hot day out today")
+elif temperature <= 79 and temperature >= 60:
+    print("its a nice day today")
+elif temperature < 60 and temperature > 40:
+    print("its cold today")
+else:
+    print("its freezing outside")
+'''
+# not good because the except will trigger on any error and not just the text error
+#if you know something will cause an error but dont know what kind run it and then add the type error
+
+# try / except
+'''
+try:
+    dict = {}
+    dict["potato"]
+except KeyError:
+    print("this key does not exist.")
+'''
+# dictionaries {}
+
+#keys on left value on right 
+example = {}# makes  a dictionarie
+'''
+foods = {
+    "apple": 1,
+    "orange": 1.5,
+    "banana":1,
+    "spinach":2
+}
+
+foods["tomato"] = 3#you can add to your dict anywhere below you can also update values this way
+print(foods)#prints whole food dict
+print(foods["orange"])#prints the value of orange
+foods["banana"] = 2
+print(foods["tomato"])#useing something you dont have causes a key error
+del foods["spinach"]# deletes key and value
+print(foods)
+#adding food to the dict in the program
+'''
+'''
+new_food = input("enter a food to add or 'done' to exit: ")
+food_price = input(f"enter the price of {new_food}: ")
+food_price = float(food_price)
+foods[new_food] = food_price
+'''
+'''
+print(foods)
+
+while True:
+    new_food = input("enter a food to add or 'done' to exit: ")
+    if new_food == "done":
+        break
+    food_price = float(input(f"enter the price of {new_food}: "))
+
+    foods[new_food] = food_price
+    print(foods)
+
+food_lookup = input("enter a food name to get the price.")
+try:
+    reply = foods[food_lookup]
+except KeyError:
+    print(f"{food_lookup} does not exist")
+else:
+    print(f"the price of {food_lookup} does not exist")
+
+'''
+#lists []
+foods =["potato","tomato","grape","kiwi"]
+print(foods[1])
+#first thing in a list is at index 0
+#negative values start at the bottom of the list
+#putting in a index that doesnt exist causes a IndexError
+foods.append("pineapple")#adds pineapple to end of the list
+#foods[0] = "apple" # overwrites whatever was in the specifed index 
+
+
+#removed_item = foods.pop(1)#save removed items for use later
+#foods.remove("grape")#removes item and returns nothing
+
+if "potato" in foods:
+    print("hey! Potato isnt a fruit!")
+else:
+    print("only fruits here :)")
+
+#print(foods[0])
+#print(foods[1])
+#print(foods[2])
+#print(foods[3])
+
+#for food in foods:#^does the same thing 
+#    print(food)
+
+
+#for i in range(len(foods)):#^same thing prints items in the range of the length of the foods list
+#    print(foods[i])
+
+#print(foods[1:2])#get all the foods from index 1 and up to but not including index 2 
+#print(foods[0:3:2])#3rd number is the step and its how many indexes you move at a time
+#print(foods[::-1])#prints list backwords if left blank it does entire list
+#[start:stop:step]
