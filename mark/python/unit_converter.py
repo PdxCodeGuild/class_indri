32bc254b5a63d1b63b4aab9bddcbe8f7ef445695
Mark <67750958+Mftodd@users.noreply.github.com>
2022-10-19 20:36:34 -0700

    unit converter version 2&3

--- mark/python/unit_converter.py
+++ mark/python/unit_converter.py
@@ -1,15 +1,23 @@
 # Unit Converter
 
-# Dictionary containing conversion of unit to meters
+# Dictionary containing conversion of unit to meters, with support for yards and inches
 convert = {
-    "ft" : 0.3048
+    "ft" : 0.3048,
+    "mi" : 1609.34,
+    "m" : 1,
+    "km" : 1000,
+    "yd" : 0.9144,
+    "in" : 0.0254
 }
 
-# Ask the user for the number of feet
+# ask the user for the number
 user_distance = input("what is the distance in feet? ")
 
-#Convert the user distance from feet to meters
-result = int(user_distance) * convert["ft"]
+# ask the user for the unit
+user_unit = input("What are the units? ")
+
+#Convert the user distance from user unit to meters
+result = int(user_distance) * convert[user_unit]
 
 #return the result
 print(result)
