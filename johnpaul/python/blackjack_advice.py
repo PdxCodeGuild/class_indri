379dfe42cfe9083accffc73f396a06f4ad27fefd
neshure <jpbiztalk@yahoo.com>
2022-10-20 23:33:26 -0500

    updated unit-converter ex and added blackjack advice lab

---
+++ johnpaul/python/blackjack_advice.py
@@ -0,0 +1,21 @@
+print("\nBlackjack Advice")
+print("===============================================")
+print("""
+\nAvailable card Input: 
+-----------------------
+A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K
+""")
+
+card_dict = {'A': 1, '2': 2, '3' :3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10 , 'J': 10, 'Q': 10, 'K': 10}
+
+
+card_input_1 = input("What's your first card? ").upper()
+card_input_2 = input("What's your second card? ").upper()
+card_input_3 = input("What's your third card? ").upper()
+ 
+
+
+ 
+
+
+
