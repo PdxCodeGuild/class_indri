7bd7d561c035d7664c7b88ebb6ed0baba7c44bee
Mark <67750958+Mftodd@users.noreply.github.com>
2022-10-20 22:43:34 -0700

    blackjack_advice with version 2

--- mark/python/blackjack_advice.py
+++ mark/python/blackjack_advice.py
@@ -1,9 +1,14 @@
 # Let's write a python program to give basic blackjack playing advice during a game by asking the player for cards. 
 
+
 # Ask the user for three playing cards (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K). 
+
 first_card = input("What's your first card? ")
 second_card = input("What's your second card? ")
 third_card = input("What's your third card? ")
+# Store cards in a list for later recall
+user_hand = [first_card, second_card, third_card]
+
 
 # Dictionary containing value of each card individually. 
 # Number cards are worth their number, all face cards are worth 10. 
@@ -24,9 +29,22 @@ card_value = {
     "A" : 1
 }
 
+
 # Calculate the sum of the three cards to get the total point value
 score = card_value[first_card] + card_value[second_card] + card_value[third_card]
-# print(score) #test
+
+
+# check for Aces and add points so long as it won't cause you to lose 
+while "A" in user_hand and score <= 12:
+    # score is added with ace = 1, add 9 to get 10 points.
+    # Aces are only counted as 10 points one at a time.
+    score = score + 9
+    # remove one ace from the user's hand after they get points for it
+    user_hand.remove("A")
+    # print(user_hand) #test #success!
+
+# print(score) #test # success!
+
 
 # Create a function that returns advice
 def Blackjack(advice):
