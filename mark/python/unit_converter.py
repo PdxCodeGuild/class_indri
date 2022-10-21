5c5b0c537a7a576f14d20fce117ae9bf51a6d677
Mark <67750958+Mftodd@users.noreply.github.com>
2022-10-20 19:42:25 -0700

    need to commit unit converter again to resolve error changing branches.

--- mark/python/unit_converter.py
+++ mark/python/unit_converter.py
@@ -11,23 +11,21 @@ convert = {
     "in" : 0.0254
 }
 
+while True:
+    # ask the user for the number
+    user_distance = input("what is the distance? ")
 
-# ask the user for the number
-user_distance = input("what is the distance? ")
+    # ask the user for the unit
+    from_unit = input("What are the input units? ")
 
-# ask the user for the unit
-from_unit = input("What are the input units? ")
+    # ask the user what to convert to
+    to_unit = input("what are the output units? ")
 
-# ask the user what to convert to
-to_unit = input("what are the output units? ")
+    # Convert the user distance from user unit to meters
+    meters = int(user_distance) * convert[from_unit]
 
-# Convert the user distance from user unit to meters
-meters = int(user_distance) * convert[from_unit]
+    # Convert from meters to the desired unit
+    result = meters * (1/convert[to_unit])
 
-# Convert from meters to the desired unit
-result = meters * (1/convert[to_unit])
-
-#return the result
-print(f"{user_distance} {from_unit} is {result} {to_unit}")
-
-
+    #return the result
+    print(f"{user_distance} {from_unit} is {result} {to_unit}")
