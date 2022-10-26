60519b8f2de5811d86b99ac202b69e34ce3b306c
IcyStab <87219008+IcyStab@users.noreply.github.com>
2022-10-25 23:30:13 -0500

    updated blackjack advice and added the start of pick6 lab

--- noah-initial/python/blackjack_advice.py
+++ noah-initial/python/blackjack_advice.py
@@ -4,7 +4,6 @@ By:Noah Wallis
 
 """
 
-
 card_values = {
     "A": 1,
     "2": 2,
@@ -21,22 +20,26 @@ card_values = {
     "K": 10
 }
 
-play_again = True
+
 card_integer_values = []
 
-card_one = input("What is your first card?: ")
-card_integer_values.append(card_values[card_one])
+while True:
+    try:
+        card_one = input("What is your first card?: ").upper()
+        card_integer_values.append(card_values[card_one])
+        
+        card_two = input("What is your second card?: ").upper()
+        card_integer_values.append(card_values[card_two])
+        
+        card_three = input("What is your third card?: ").upper()
+        card_integer_values.append(card_values[card_three])
+        break
 
-card_two = input("What is your second card?: ")
-card_integer_values.append(card_values[card_two])
-
-card_three = input("What is your third card?: ")
-card_integer_values.append(card_values[card_three])
-
+    except KeyError:
+        print("That is not a valid card, try again!")
+        
 
 sum_of_cards = card_integer_values[0] + card_integer_values[1] + card_integer_values[2]
-
-
 print(f"The total number of points you have is {sum_of_cards}!")
 
 
