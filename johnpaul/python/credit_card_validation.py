7298a2c7f99ee7bb4767176f0c459525ec906b70
neshure <jpbiztalk@yahoo.com>
2022-10-29 15:18:31 -0500

    complete credit card validation lab

--- johnpaul/python/credit_card_validation.py
+++ johnpaul/python/credit_card_validation.py
@@ -2,49 +2,50 @@
 
 
 # user input credit card digits
-user_input = "455673758689855"
+user_input = input("Enter your credit cared number: ")
 # print(type(user_input))
 
 #Convert the input string into a list of ints
-user_input = list(user_input)
+user_input = list(map(int, user_input))
 print(user_input)
 
-# Slice off the last digit. That is the check digit.
-def valid_credit_card(user_input):
-    sliced_off_digit = user_input[: -1]
-    return sliced_off_digit
 
-result = valid_credit_card(user_input)
-# print(result)
+
+# Slice off the last digit. That is the check digit.
+
+popped_digit = user_input.pop()
+print(user_input)
 
 # Reverse the digits. 
 def digit_reversed(user_input): 
     new_list = user_input[::-1]
     return new_list
 
-new_result = digit_reversed(result)
+new_result = digit_reversed(user_input)
 print(new_result)
 
-
-
-'''We can get sublists using square brackets and colons [::], this is called slicing. 
-# The first number is the starting index, the second is the ending index, and the third is the increment. 
-# If you leave out the first number, it's implied to be the beginning. 
-# If you leave out the second number it's implied to be the end. 
-# If you leave out the third number, it's implied to be 1'''
-
 # Double every other element in the reversed list.
-num = []
-for x in new_result[::2]:
-    x = int(x)
-    x = num.append(x * 2)
-
-print(num)
-
+for i in range(len(new_result)):
+    if i % 2 == 0:
+        new_result[i] = new_result[i]*2
+        # print(new_result)
+      
+print(new_result)       
 # Subtract nine from numbers over nine.
+for x in range(len(new_result)):
+    if new_result[x] > 9:
+        new_result[x] = new_result[x] - 9
+#        print(sum(new_result)) # Sum all values.
 
-# Sum all values.
-
+print(new_result)
+print(sum(new_result))
 # Take the second digit of that sum.
+net = (sum(new_result)) % 10
+print(net)
 
 # If that matches the check digit, the whole card number is valid.
+final_check = net - popped_digit
+if final_check == 0:
+    print("Valid!")
+else:
+    print("Invalid")
