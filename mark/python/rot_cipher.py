6761a2c865c451b96ead62b2065394bf740a2884
Mark <67750958+Mftodd@users.noreply.github.com>
2022-11-01 21:47:59 -0700

    rot cipher version 3

--- mark/python/rot_cipher.py
+++ mark/python/rot_cipher.py
@@ -1,66 +1,52 @@
-'''Rot Cipher
-Write a program that prompts the user for a string, and encodes it with ROT13. 
-
+'''Write a program that prompts the user for a string, and encodes it with ROT13. 
 For each character, find the corresponding character, add it to an output string. 
-
-Notice that there are 26 letters in the English language, so encryption is the same as decryption.
-
-Index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
-English	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
-ROT+13	n	o	p	q	r	s	t	u	v	w	x	y	z	a	b	c	d	e	f	g	h	i	j	k	l	m
-
-
-
-Version 2
-
-Allow the user to input the amount of rotation used in the encryption. (ROTN)
-
-Version 3 (optional)
-
-Add support for capital letters, numbers, and special characters. These can be handled in two different ways:
-
-Capital letters can be rotated as well, numbers and special characters can be put directly into the output (e.g. "hello world!" becomes "uryyb jbeyq!").
-
-Instead of using an alphabet of just letters, include numbers, spaces, and special characters as well.'''
+Notice that there are 26 letters in the English language, so encryption is the same as decryption.'''
 import string
 
-miniscule = list(string.ascii_lowercase)
-capitalized = list(string.ascii_uppercase)
-# print(alphabet)
+lower_case = list(string.ascii_lowercase) #support for lowercase characters
+upper_case = list(string.ascii_uppercase) #support for uppercase characters
 
-#take in a letter, return a letter 13 places to the right]
-def rot_cipher(letter):
-    
-    if letter in miniscule:
-        letter_index = miniscule.index(letter)
-        try:
-            rotation = miniscule[letter_index + 13]
+# take in a character & if a letter, return a rotated letter according to the chosen step.
+def rot_cipher(character, step):
+    # check for letter case & if the letter is supported,     
+    if character in lower_case:     
+        # take the index position of the letter,
+        letter_index = lower_case.index(character)    
+        # apply the step
+        try:  
+            rotation = lower_case[letter_index + int(int(step))] # return value at position for letter index plus step.
         except IndexError:
-            rotation = miniscule[(letter_index+13) % len(miniscule)]
+            rotation = lower_case[(letter_index+int(step)) % len(lower_case)] #if > length of list, divide length of list from total & return index value.  
+        # return the value at the stepped index position
         return rotation
-
-    if letter in capitalized:
-        letter_index = capitalized.index(letter)
+    #samesies but for upper case
+    if character in upper_case:
+        letter_index = upper_case.index(character)
         try:
-            rotation = capitalized[letter_index + 13]
+            rotation = upper_case[letter_index + int(step)]
         except IndexError:
-            rotation = capitalized[(letter_index+13) % len(capitalized)]
+            rotation = upper_case[(letter_index + int(step)) % len(upper_case)]
         return rotation
-
+    # numbers and special characters can be put directly into the output
     else:
-        return letter
+        return character
 
 # print(rot_cipher("l"))
 
-user_input = input("Enter a word or phrase: ")
+user_phrase = input("Enter a word or phrase: ")
+user_step = input("Input the amount of rotation: ")
 
+# list of rotated letters
 cipher_list = []
 
-for i in user_input:
-        cipher_list.append(rot_cipher(i))
+# run every position in the input string through the rot_cipher function & add the result to our list of rotated letters
+for i in user_phrase:
+        cipher_list.append(rot_cipher(i, user_step))
 
+# join the list into a string
 output = "".join(cipher_list)
 
+#return the encrypted string
 print(output)
 
 
