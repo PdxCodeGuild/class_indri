435f2212744b59cfdf8d4eeeadd70bd500374eb8
heirquiles <106492189+heirquiles@users.noreply.github.com>
2022-11-03 20:18:28 -0700

    count_words.py V1 & V2 created

--- victor/python/word_count.py
+++ victor/python/word_count.py
@@ -8,16 +8,22 @@ book = (response.text).lower()
 # Take the following steps to build up our dictionary. The result should look something like {'a': 3, 'the': 4}
 
 # Make everything lowercase, strip punctuation, split into a list of words.
-book = book.translate(str.maketrans('', '', string.punctuation))
+punct = string.punctuation + '' + '//' 
+book = book.translate(str.maketrans('', '', punct))
 book_list = book.split(" ")
 
 # Your dictionary will have words as keys and counts as values. If a word isn't in your dictionary yet, add it with a count of 1. If it is, increment its count.
 word_dict = {}
-for word in book_list:
-    if word not in word_dict:
-        word_dict[word] = 1
+
+
+word_pair_list = [book_list[i:i+2] for i in range(0, len(book_list), 2)]
+
+for word_pair in word_pair_list:
+    word_pair = tuple(word_pair)
+    if word_pair not in word_dict:
+        word_dict[word_pair] = 1
     else:
-        word_dict[word] += 1
+        word_dict[word_pair] += 1
 
 # Print the most frequent top 10 out with their counts. You can do that with the code below.
 
@@ -26,3 +32,6 @@ words = list(word_dict.items()) # .items() returns a list of tuples
 words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
 for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
     print(words[i])
+    
+
+# Count how often each unique pair of words is used, then print the top 10 most common pairs with their counts.
