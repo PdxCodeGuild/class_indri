7068c837fb346d47ee549b3025f8fda9cae9463e
neshure <jpbiztalk@yahoo.com>
2022-11-08 18:23:10 -0600

    updated rot cipher to match solution

--- johnpaul/python/rot_cipher.py
+++ johnpaul/python/rot_cipher.py
@@ -1,45 +1,20 @@
-from dataclasses import replace
 import string
 
-string_ascii_list = list(string.ascii_lowercase)
+# get string from user
+user_phrase = input("Enter a word or phrase to encrypt: ")
 
-#print(string_ascii_list)
+# rotate string by 13 characters
+user_phrase = user_phrase.lower()
+letters = string.ascii_letters + string.digits + string.punctuation    # "abcdefghijklmnopqrstuvwxyz"
+shift = int(input("Enter the amount to rotate: "))
+output = ""
+for char in user_phrase:
+    if char in letters:
+        position = letters.index(char) # d => 3
+        new_position = (position + shift) % len(letters) # 3 + 13 => 16
+        output += letters[new_position] # letters[16] => q
+    else:
+        output += char
 
-
-# 1) Prompts the user for a string
-
-prompt = input("Enter letters: ")
-
-prompt = prompt.replace(" ", "") # remove spaces if any
-
-# Allow the user to input the amount of rotation used in the encryption. (ROTN)
-user_rotn_input = int(input("input number of rotation: "))
-
-prompt_list = list(prompt) # convert to list of string
-
-
-
-# 2) Encodes it with ROT13
-
-
-for char in prompt_list:
-    output =[]
-    output += [string_ascii_list.index(char)]
-    for num in output:
-        # print(num)
-# 3) For each character, find the corresponding character, add it to an output string
-        if num + user_rotn_input < 26:
-            print(string_ascii_list[num + user_rotn_input], end="\n")
-        if num + user_rotn_input > 26:
-            print(string_ascii_list[num + user_rotn_input - 26], end="\n")
-
-
-
-
-# Version 2: 
-## 
-        
-
-
-
-
+# output the rotated string
+print(output)
