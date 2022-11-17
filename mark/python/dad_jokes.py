fd2aea034002c7221e0b28bc032654a88479ff93
Mark <67750958+Mftodd@users.noreply.github.com>
2022-11-16 21:37:38 -0800

    dad_jokes v2

--- mark/python/dad_jokes.py
+++ mark/python/dad_jokes.py
@@ -1,7 +1,26 @@
 import requests
-url = "https://icanhazdadjoke.com/"
-response = requests.get(url, headers={'accept':'application/json'}).json()
 
-joke = response["joke"]
+search_term = input("Enter a search term: ")
+page_num = 1 
+keep_playing = True
+while keep_playing == True:
+    url = f"https://icanhazdadjoke.com/search?term={search_term}&page={page_num}"
+    response = requests.get(url, headers={'accept':'application/json'}).json()
 
-print(joke)
+    #support for multiple pages
+    if page_num > response['total_pages']: 
+        print("End of the line! Goodbye.")
+        break
+    else:
+        page_num += 1
+
+    # return joke from each result one at a time
+    for i in response['results']:
+        print(f"\n{i['joke']}\n")
+        # check to see if we want to keep going
+        next_joke = input("Would you like to get another? y/n: ").lower()
+
+        # end parent While loop to quit playing
+        if next_joke != "y":
+            keep_playing = False
+            break
