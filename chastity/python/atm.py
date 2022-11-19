67247f24f501865325a6bd6b3b14c3a5676e6e6b
BoykinChas <115953792+BoykinChas@users.noreply.github.com>
2022-11-18 21:47:50 -0600

    ATM V1 & V2 Completed

--- chastity/python/atm.py
+++ chastity/python/atm.py
@@ -10,28 +10,31 @@ class ATM:
     
     def __init__(self):
         # initialize our class with a balance of 0 and an interest rate of 0.1
-        self.balance = 0
+        self.account = 0
         self.interest = 0.1
         self.transactions = []
     
     def balance(self):
         # return the account balance
-        self.balance 
+        return self.account 
     
     def deposit(self, amount):
         # add the given amount to the balance
-        self.transactions.append (f'Personal Deposit ${amount}')
-        self.balance += amount
+        self.transactions.append(f'Personal Deposit ${amount}')
+        self.account += amount
     
     def check_withdrawal(self, amount):
         # returns true if the withdrawn amount won't put the account in the negative, false otherwise
-        return self.balance > amount
+        if amount <= self.account:
+            return True
+        else:
+            return False
     
     def withdraw(self, amount):
         # removes the given amount from the balance and returns it
         if self.check_withdrawal(amount):
             self.transactions.append(f'Personal Withdral ${amount}')
-            self.balance -= amount
+            self.account -= amount
             return amount
         else:
             self.transactions.append(f'Personal withdrawl unsucessful ${amount}')
@@ -40,7 +43,7 @@ class ATM:
     def deposit_interest(self):
         # calculate the amount of interest accumulated and add it to our balance
         # return the amount of interest added
-        interest = self.balance * 0.1 
+        interest = self.account * 0.1 
         return interest
 
     def print_transactions(self):
@@ -70,6 +73,10 @@ while True:
         amount = atm.deposit_interest() # call the calc_interest() method
         atm.deposit(amount)
         print(f'Deposited ${amount} in interest')
+    elif command == 'transactions':  
+        print('Recent Transactions:\n') # Print list of user transactions
+        for transaction in atm.transactions:
+            print(transaction)
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
