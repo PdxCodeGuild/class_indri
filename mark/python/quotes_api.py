1e3cd1758e14acc9dfc37281aa4117d9fd398ba2
Mark <67750958+Mftodd@users.noreply.github.com>
2022-11-18 21:10:05 -0800

    updated quotes api & added comments

--- mark/python/quotes_api.py
+++ mark/python/quotes_api.py
@@ -5,25 +5,43 @@ import json
 
 # The Favqs Quote API also supports getting a list of quotes associated with a given keyword https://favqs.com/api/quotes?page=<page>&filter=<keyword>
 
+# page counter and search term
 page = 1
 keyword = input("Enter a keyword to search for quotes: ")
 
+# run loop
 while True:
-    
+
     url = f"https://favqs.com/api/quotes?page={page}&filter={keyword}"
     response = requests.get(url, headers={'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}).json()
     
     # print(json.dumps(response, indent=0)) # inspect return data 
+    # break
+
+    # return the quotes with authors
     for i in response['quotes']:
         print(f"\n{i['body']}\n–– {i['author']}\n")
 
-    fork = input("Enter 'next page', 'new search' or 'done': ").lower()
-    
-    if fork == 'next page':
-        page += 1
-    elif fork == 'new search':
-        keyword = input("Enter a keyword to search for quotes: ")
+    # keep going, search again or exit
+    # if on the last page, don't suggest to search the next page.
+    if response['last_page'] == True:
+        fork = input("Last page! Enter 'new search' or 'done': ").lower()
+        page = 1 # reset page count  
     else:
+        fork = input("Enter 'next page', 'new search' or 'done': ").lower()
+    
+    # multiple interpretations
+    next_page = ['next page', 'next', 'nex', 'nxt', 'np']
+    new_search = ['new search', 'new','search', 'ns']
+    
+    if fork in next_page and response['last_page'] == True: # loop back to the first page if the user types next page while on the last page
+        continue
+
+    if fork in next_page: # loop on the next page
+        page += 1
+    elif fork in new_search: # loop with a new search term
+        keyword = input("Enter a keyword to search for quotes: ")
+    else:                 # exit loop
         print("Goodbye!")
         False
         break
