9f0a15edd930bf3316151418db5dc0d5b5597058
BoykinChas <115953792+BoykinChas@users.noreply.github.com>
2022-12-20 01:06:08 -0600

    company lab complete

---
+++ chastity/Django/polls/polls/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Models are a pythin representation of a sql table
+# Each class is a new table
+# Properties on the call are columns
+# We assign the type of data to each property
+class Question(models.Model):
+    question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"({self.votes}) -- {self.choice_text}"
