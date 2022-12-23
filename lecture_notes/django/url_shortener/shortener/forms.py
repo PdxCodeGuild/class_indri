5157856fbc8d40d8aaf566e0b271922788dede4b
Anthony Wallace <anthonyt.wallace@icloud.com>
2022-12-22 18:45:51 -0800

    Added comments to forms.py

--- lecture_notes/django/url_shortener/shortener/forms.py
+++ lecture_notes/django/url_shortener/shortener/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
-
+# Create a new form class called URLForm
 class URLForm(forms.Form):
+    # Define a 'url' field as a URLField with a maximum length of 2048 characters
+    # The field is rendered as a text input with a placeholder attribute
     url = forms.URLField(label="", max_length=2048, widget=forms.TextInput(
         attrs={'placeholder': 'Enter url to be shortened'}))
