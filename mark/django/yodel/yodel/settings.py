ede03013d0c9b248ba21598c8d3eab79a7f5b8f4
Mark <67750958+Mftodd@users.noreply.github.com>
2023-01-06 20:07:55 -0800

    updated twitter clone

--- mark/django/yodel/yodel/settings.py
+++ mark/django/yodel/yodel/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
