9fe7ec79db35f199642fb5901fcbf6b4936a4a0b
Mark <67750958+Mftodd@users.noreply.github.com>
2023-01-06 22:35:57 -0800

    compeleted redo lab

--- mark/javascript/redo/main.js
+++ mark/javascript/redo/main.js
@@ -1,8 +1,8 @@
 // Pick a Python lab and re-do it in JavaScript. 
 // You should first try to write them using JavaScript's prompt and alert in place of Python's input and print.
 
-// unit_converter
 
+// unit_converter
 let convert = {
     "ft" : 0.3048,
     "mi" : 1609.34,
@@ -12,14 +12,13 @@ let convert = {
     "in" : 0.0254
 }
 
-
-// # ask the user for the number
+// # ask the user for the value
 const value = prompt("Enter a value to convert to meters: ")
 
 // # ask the user for the unit
 const unit = prompt("Enter a unit (ft, mi, km, yd, in): ")
 
-// # Convert the user distance from user unit to meters
+// # Convert the user value from user unit to meters
 let result = (value * convert[unit])
 
 // #return the result
