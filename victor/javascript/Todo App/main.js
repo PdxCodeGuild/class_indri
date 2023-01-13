dfd36537cc3c886e70d38a8dff4aee95c6426b18
heirquiles <106492189+heirquiles@users.noreply.github.com>
2023-01-12 21:14:29 -0800

    Debugged and updated styling

--- victor/javascript/Todo App/main.js
+++ victor/javascript/Todo App/main.js
@@ -11,22 +11,14 @@ btn.addEventListener('click', function(){
     todo.textContent = input.value + " "
     parent.appendChild(todo)
     todo.appendChild(todoBtn)
-    let doneBtn = document.querySelector('.todo-btn')
-    doneBtn.addEventListener('click', function(){
+    todoBtn.className = 'todo-btn'
+    
+    todoBtn.addEventListener('click', function(){  
+        console.dir(this)
         todo.removeChild(todoBtn)
         doneList.appendChild(todo)
-        parent.removeChild(todo)
     })
 
     input.value = ""
 })
-
-// todoItem.addEventListener('click', function(){
-//     todos.forEach();{
-//         // somehow get the button generated from the todo input to remove its own element from the todo list and move it to the done list, when clicked
-//         // if (){}
-
-//     }
-
-// })
 
