7876408c9b0304f7299cff7fe8719c491ece64e9
neshure <jpdevopsvm@gmail.com>
2023-01-13 00:27:53 -0600

    updated todo with remove btn and working on completedBtn

---
+++ johnpaul/javascript/labs/todo-list/scripts.js
@@ -0,0 +1,69 @@
+// Removed items should disappear entirely. 
+// Completed items should appear at the bottom (or in a separate list) with a line through them
+
+
+// add an item to the list
+let textArea = document.querySelector('#inputarea')
+let addBtn = document.querySelector('#addBtn')
+let removeBtn = document.querySelector('#removeBtn')
+let completedBtn = document.querySelector('#completedBtn')
+let containerRow = document.querySelector('#containerRow')
+let completedList = document.querySelector('#completedList')
+
+function todo(){
+  addBtn.addEventListener('click', function(){
+    //  create checkbox element
+    let checkbox = document.createElement('input');
+    checkbox.id = 'checkedId';
+    checkbox.type = 'checkbox';
+    // checkbox.addEventListener('click', function(){
+    //   console.dir(checkbox)
+    // })
+    // creating a paragraph element
+    let paragraph = document.createElement('p');
+    paragraph.innerText = inputarea.value;
+    paragraph.id = "todoparagraph"
+    // paragraph.addEventListener('click', function(){
+    //   console.dir(paragraph)
+    // })
+  
+    let myDiv = document.createElement('div')
+    myDiv.appendChild(checkbox);
+    myDiv.appendChild(paragraph);
+    // append the value to the <div> id=containerRow
+    containerRow.appendChild(myDiv);
+    // clears the textarea after adding input
+    inputarea.value = "";
+  })
+  
+  // remove an item from the list
+  removeBtn.addEventListener('click', function(){
+    let itemChecked = document.querySelectorAll('#checkedId')
+    for (let i = 0; i < itemChecked.length; i++) {
+      if (itemChecked[i].checked == true) {
+        // console.dir(itemChecked[i])
+        containerRow.removeChild(itemChecked[i].parentElement)
+      }
+    }
+  })
+  // mark an item as completed
+  completedBtn.addEventListener('click', function(){
+    let itemChecked = document.querySelectorAll('#checkedId')
+    console.log(itemChecked);
+    for (let i = 0; i < itemChecked.length; i++) {
+      if (itemChecked[i].checked == true) {
+        containerRow.removeChild(itemChecked[i].parentElement)
+        // let completedList = document.createElement('div')
+        console.log(itemChecked[i].parentElement.innerText.concat(' => completed'));
+      }
+    }
+  })
+}
+
+todo()
+
+
+
+
+
+
