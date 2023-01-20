e78a4d0d102da6f49279a9c8063e7ac279a91833
Mark <67750958+Mftodd@users.noreply.github.com>
2023-01-19 22:46:59 -0800

    updated weather api lab

--- mark/javascript/weather_api/main.js
+++ mark/javascript/weather_api/main.js
@@ -1,24 +1,80 @@
+let userLocation = document.querySelector('#user-location')
+let outsideTemp = document.querySelector('#outside-temp')
+let weatherConditions = document.querySelector('#weather-conditions')
+let windConditions = document.querySelector('#wind-conditions')
 
 
 
+const compass = [
+        {minDegree: 0, maxDegree:22.5 , direction: "N"},
+        {minDegree: 22.6, maxDegree: 67.5, direction: "NE"},
+        {minDegree: 67.6, maxDegree:112.5 , direction: "E"},
+        {minDegree: 112.6, maxDegree:157.5 , direction: "SE"},
+        {minDegree: 157.6, maxDegree:202.5 , direction: "S"},
+        {minDegree: 202.6, maxDegree:247.5 , direction: "SW"},
+        {minDegree: 247.6, maxDegree:292.5 , direction: "W"},
+        {minDegree: 292.6, maxDegree:337.5 , direction: "NW"},
+        {minDegree: 337.6, maxDegree:360 , direction: "N"},
+]
 
-navigator.geolocation.getCurrentPosition(returnPosition)
 
-function returnPosition(position) {
-    lat = position.coords.latitude
-    long = position.coords.longitude
-    console.log(`Lat: ${lat} Long: ${long}`)
-}
 
+navigator.geolocation.getCurrentPosition(returnWeather)
+
+function returnWeather(position) {    
     axios({
         method: 'post',
         url: 'https://api.openweathermap.org/data/2.5/weather',
         params: {
-            lat : 35.962719796966304,
-            lon : -114.84043915487283,
+            lat : position.coords.latitude,
+            lon : position.coords.longitude,
             appid : weather_key
         }})
-    .then(function(data) {
-        console.log(data.data.main.temp)
-    })
+        .then(function(data) {
+            // return data
+            let weatherData = (data.data)
+            
+            // location name
+            userLocation.textContent = weatherData.name
 
+
+            // temps
+            let tempKelvin = weatherData.main.temp
+            let tempCelsius = (Math.round(tempKelvin - 273.15))
+            let tempFahrenheit = (Math.round(tempCelsius) * 9 / 5 + 32)
+            
+            outsideTemp.textContent = tempFahrenheit
+            
+            // weather
+            weatherConditions.textContent = weatherData.weather[0].description
+            
+            // wind
+            let windSpeed = weatherData.wind['speed']
+            let windDeg = Math.abs(weatherData.wind['deg'])
+            let windDirection = findDirection(windDeg)
+
+            windConditions.textContent = `Wind Speed: ${windSpeed} Direction: ${windDirection}`
+
+            console.log(weatherData)
+        })
+}
+
+// 
+
+
+function findDirection(heading) {
+    heading = Math.abs(heading % 360)
+
+        compass.forEach((object) => {
+
+            if (heading == object.minDegree || heading == object.maxDegree){
+                console.log(object.direction)
+                return object.direction;}
+
+            if (heading > object.minDegree && heading < object.maxDegree) {
+                console.log(object.direction)
+                return object.direction;}
+            
+            }
+        )
+    }
