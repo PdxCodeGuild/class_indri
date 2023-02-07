9f7dbdbefacade271b2163580a9ba954f9100bef
BoykinChas <115953792+BoykinChas@users.noreply.github.com>
2023-02-07 17:05:51 -0600

    finished lab

--- chastity/javascript/vue/main.js
+++ chastity/javascript/vue/main.js
@@ -1,32 +1,33 @@
 const { createApp } = Vue
 
-const app = createApp({
 
-    data() { 
+const app = createApp({
+    
+    data() {
         return {
-            choice: ["🪨", "📃", "✂️"],
-            playChoice: null,
+            options: ["🪨", "📃", "✂️"],
+            playerChoice: null,
             result: ""
         }
     },
     methods: {
-      play(playChoice) {
-        let compChoice = Math.floor(Math.randon() * 3)
-        compChoice = 3[compChoice]
-        if (playChoice === compChoice){
-          this.result = "It is a tie"
-          this.playChoice = playChoice
-          this.compChoice = compChoice
-        } else if (playChoice === "🪨" && compChoice === "📃" || playChoice === "📃" && compChoice === "✂️" || playChoice === "✂️" && compChoice === "🪨"){
-          this.result = "You Lose"
-          this.playChoice = playChoice
-          this.compChoice = compChoice
-        } else {
-          this.result = "YOU WIN!!!"
-          this.playChoice = playChoice
-          this.compChoice = compChoice
+        play(playerChoice) {
+            let computerChoice = Math.floor(Math.random() * this.options.length)
+            computerChoice = this.options[computerChoice]
+            if (playerChoice == computerChoice){
+                this.result = "It is a tie!"
+                this.playerChoice = playerChoice
+                this.computerChoice = computerChoice
+            } else if (playerChoice == "🪨" && computerChoice == "📃" || playerChoice == "📃" && computerChoice == "✂️" || playerChoice == "✂️" && computerChoice == "🪨"){
+                this.result = "You Lose!"
+                this.playerChoice = playerChoice
+                this.computerChoice = computerChoice
+            } else if (playerChoice == "📃" && computerChoice == "🪨" || playerChoice == "✂️" && computerChoice == "📃" || playerChoice == "🪨" && computerChoice == "✂️") {
+                this.result = "YOU WIN!!"
+                this.playerChoice = playerChoice
+                this.computerChoice = computerChoice
+            }
         }
-      }
     }
 })
 
